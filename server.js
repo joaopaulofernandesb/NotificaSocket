2719593da4411806e4bb62d5dfe67e3a3843b840
Joao Brasil <joao_brasil@TSPD-NB-00475.tecnospeed.local>
2020-02-03 16:01:13 -0300

    ajuste

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express"),
+  app = express(),
+  server = require("http")
+    .createServer(app)
+    .listen(4555),
+  io = require("socket.io").listen(server),
+  bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = 3000;
+var router = express.Router();
+/* Socket irá aqui depois */
+var emitir = function(req, res, next) {
+  var notificar = req.query.notificacao || "";
+  if (notificar != "") {
+    io.emit("notificacao", notificar);
+    next();
+  } else {
+    next();
+  }
+};
+app.use(emitir);
+app.use("/api", router);
+router.route("/notificar").get(function(req, res) {
+  //aqui vamos receber a mensagem
+  res.json({ message: "Rota Ok" });
+});
+
+app.listen(port);
+console.log("conectado a porta " + port);
