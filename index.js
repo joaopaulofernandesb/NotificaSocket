8199324be45ae618038e0e996be17f9a7e1b40ff
Joao Brasil <joao_brasil@TSPD-NB-00475.tecnospeed.local>
2020-03-05 08:49:31 -0300

    ajustando o socket

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 require("dotenv").config();
 var express = require("express"),
   cors = require("cors");
-(app = express()), app.use(cors());
-(server = require("http")
-  .createServer(app)
-  .listen(3002)),
-  (io = require("socket.io").listen(server)),
-  (bodyParser = require("body-parser"));
+const app = express();
+
+app.use(cors());
+
+const server = require("http").Server(app);
+
+const io = require("socket.io")(server);
+
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = 8080;
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 var router = express.Router();
 /* Socket irá aqui depois */
 var emitir = function(req, res, next) {
   var notificar = req.query.notificacao || "";
+  var teste = req.query.teste || "";
+
   if (notificar != "") {
     io.emit("notificacao", notificar);
     next();
   } else {
     next();
   }
+
+  if (teste != "") {
+    io.emit("teste", teste);
+    next();
+  } else {
+    next();
+  }
 };
+
 app.use(emitir);
 app.use("/api", router);
 router.route("/notificar").get(function(req, res) {
   //aqui vamos receber a mensagem
   res.json({ message: "Rota Ok" });
 });
+router.route("/teste").get(function(req, res) {
+  //aqui vamos receber a mensagem
+  res.json({ message: "Rota Ok" });
+});
 
-app.listen(process.env.PORT || 5000);
-console.log("conectado a porta " + port);
+server.listen(process.env.PORT || 5000);
+console.log("conectado a porta " + process.env.PORT || 5000);
